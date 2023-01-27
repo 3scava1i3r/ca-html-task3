87ef00af760a00da575c41e5523e56c705fbc716
3scava1i3r <tripathi.hritwik@gmail.com>
2023-01-27 22:20:57 +0530

    task3 kinda final work

---
+++ task3/src/App.js
@@ -0,0 +1,25 @@
+import MobileNav from "./Components/mobileNav";
+import Sidebar from "./Components/sidebar";
+import PageHeader from "./Components/pageHeader";
+import InfoForm from "./Components/infoForm";
+import ProfileForm from "./Components/profileForm";
+
+
+const App = () => {
+  return (
+    <div className="flex">
+       <Sidebar />
+         <div className="relative bg-gray-25 w-full overflow-hidden flex flex-col items-start justify-start">
+         <MobileNav />
+         <main className="self-stretch flex flex-col pt-0 px-0 pb-12 items-start justify-start gap-[32px]">
+          <PageHeader />
+          <InfoForm />
+          <div className="h-0.5 w-full bg-[#EAECF0]"></div>
+          <ProfileForm />
+         </main>
+       </div>
+    </div>
+  );
+};
+
+export default App;
